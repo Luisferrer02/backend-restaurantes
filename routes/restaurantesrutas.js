2bfbf16f4532146ae48720c490781a7bd0deb310
Luisferrer02 <luisferrer2002@gmail.com>
2025-01-21 17:11:23 +0100

    arreglo backend

--- routes/restaurantesrutas.js
+++ routes/restaurantesrutas.js
@@ -45,19 +45,6 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-router.put('/:id/visita', async (req, res) => {
-  try {
-      const restaurante = await Restaurante.findById(req.params.id);
-      if (!restaurante) {
-          return res.status(404).json({ message: 'Restaurante no encontrado' });
-      }
-      restaurante.fechasVisita.push(new Date());
-      await restaurante.save();
-      res.json(restaurante);
-  } catch (error) {
-      res.status(500).json({ message: 'Error al registrar la visita', error });
-  }
-});
 
 // Obtener los detalles de un restaurante por ID
 router.get('/:id', async (req, res) => {
@@ -71,35 +58,5 @@ router.get('/:id', async (req, res) => {
     res.status(500).json({ message: 'Error al obtener el restaurante', error: err.message });
   }
 });
-
-
-router.get('/advanced', async (req, res) => {
-  try {
-      const { categorias, localizacion, sort } = req.query;
-      let query = {};
-      
-      if (categorias) {
-          query['Tipo de cocina'] = { $in: categorias.split(',') };
-      }
-      if (localizacion) {
-          query['Localización'] = localizacion;
-      }
-      
-      let restaurantes = await Restaurante.find(query);
-      if (sort) {
-          restaurantes = restaurantes.sort((a, b) => {
-              const fechaA = a.fechasVisita.length ? new Date(a.fechasVisita[0]) : new Date(0);
-              const fechaB = b.fechasVisita.length ? new Date(b.fechasVisita[0]) : new Date(0);
-              return sort === 'asc' ? fechaA - fechaB : fechaB - fechaA;
-          });
-      }
-
-      res.json(restaurantes);
-  } catch (error) {
-      res.status(500).json({ message: 'Error en la consulta avanzada', error });
-  }
-});
-
-
 
 module.exports = router;
