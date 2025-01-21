1a9a053baf87639878640c759882a23dad0fa48a
Luisferrer02 <luisferrer2002@gmail.com>
2025-01-21 11:49:23 +0100

    actualizaciones cambios nombres

--- models/Restaurante.js
+++ models/Restaurante.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const RestauranteSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  tipoCocina: { type: String, required: true },
-  localizacion: { type: String, required: true },
-  fechasVisita: { type: [Date], default: [] },
-}, { collection: 'Restaurantes' }); // Forzar el uso del nombre exacto de la colección
+  Nombre: { type: String, required: true }, // Nombre del restaurante (obligatorio)
+  'Tipo de cocina': { type: String, required: true }, // Tipo de cocina (obligatorio)
+  'Localización': { type: String, required: true }, // Localización (obligatorio)
+  Fecha: { type: String, default: '' }, // Fecha opcional
+}, { collection: 'Restaurantes' }); // Nombre explícito de la colección
 
 module.exports = mongoose.model('Restaurante', RestauranteSchema);
